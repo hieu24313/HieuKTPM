221f45140eb0118d1087bf826c6a748af47fb2fd
Hieu24313 <2051050138hieu@ou.edu.vn>
2023-02-17 13:58:36 +0700

    UI

--- KTPMApp/src/main/java/com/mycompany/ktpmapp/KTPMApp.java
+++ KTPMApp/src/main/java/com/mycompany/ktpmapp/KTPMApp.java
@@ -12,6 +12,6 @@ package com.mycompany.ktpmapp;
 public class KTPMApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello Hieu");
     }
 }
